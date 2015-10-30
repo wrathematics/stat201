3ccafc3f5a77427ff2d7918b7df3ad07377a73bd
wrathematics <wrathematics@gmail.com>
2015-10-30 15:17:26 -0400

    add input from area, validation

--- server.r
+++ server.r
@@ -2,45 +2,131 @@ library("shiny")
 library(ggplot2)
 
 
+ndigits <- 5
+round <- function(x) base::round(x, digits=ndigits)
+
+
 shinyServer(function(input, output){
+  ### Plot data
   x <- reactive({
+    validate(
+      need(!is.na(input$mean), "You must input a value for 'mean'."),
+      need(input$sd > 0, "Standard Devaiation value must be > 0")
+    )
+    
     seq(input$mean - 4*input$sd, input$mean + 4*input$sd, by=0.01)
   })
   
   df <- reactive({
-    validate(
-      need(input$sd > 0, "Standard Devaiation value must be > 0")
-    )
-    
     density <- dnorm(x(), mean=input$mean, sd=input$sd)
     data.frame(x=x(), y=density)
   })
   
+  areain <- reactive({
+    validate(
+      need(!is.na(input$areain), "You must input a value for 'area'.")
+    )
+    
+    print(input$areain)
+    input$areain
+  })
+  
+  ### above/below/etc parameters
+  above <- reactive({
+    if (input$inputtype == "value")
+      input$above
+    else if (input$inputtype == "area")
+      qnorm(mean=input$mean, sd=input$sd, areain(), lower.tail=FALSE)
+  })
+  
+  below <- reactive({
+    if (input$inputtype == "value")
+      input$below
+    else if (input$inputtype == "area")
+      qnorm(mean=input$mean, sd=input$sd, areain())
+  })
+  
+  between_low <- reactive({
+    if (input$inputtype == "value")
+      input$between_low
+    else if (input$inputtype == "area")
+      qnorm(.5 + areain()/2, mean=input$mean, sd=input$sd, lower.tail=FALSE)
+  })
+  
+  between_high <- reactive({
+    if (input$inputtype == "value")
+      input$between_high
+    else if (input$inputtype == "area")
+      qnorm(.5 + areain()/2, mean=input$mean, sd=input$sd)
+  })
+  
+  outside_low <- reactive({
+    if (input$inputtype == "value")
+      input$outside_low
+    else if (input$inputtype == "area")
+      qnorm(areain()/2, mean=input$mean, sd=input$sd)
+  })
+  
+  outside_high <- reactive({
+    if (input$inputtype == "value")
+      input$outside_high
+    else if (input$inputtype == "area")
+      qnorm(areain()/2, mean=input$mean, sd=input$sd, lower.tail=FALSE)
+  })
+  
+  
+  ### Shading region
   region <- reactive({
     if (input$areatype == "above")
-      subset(df(), x > input$above)
+      subset(df(), x > above())
     else if (input$areatype == "below")
-      subset(df(), x < input$below)
+      subset(df(), x < below())
     else if (input$areatype == "between")
-      subset(df(), x > input$between_low & x < input$between_high)
+      subset(df(), x > between_low() & x < between_high())
     else if (input$areatype == "outside")
     {
-      s <- subset(df(), x < input$outside_low | x > input$outside_high)
-      cbind(s, group=ifelse(s$x < input$outside_low, 1, 2))
+      s <- subset(df(), x < outside_low() | x > outside_high())
+      cbind(s, group=ifelse(s$x < outside_low(), 1, 2))
     }
   })
   
   area <- reactive({
+    if (input$inputtype == 'area')
+      areain()
+    else
+    {
+      validate(
+        need(!is.na(input$above), "You must input a value for 'above'."),
+        need(!is.na(input$below), "You must input a value for 'below'."),
+        need(!is.na(input$between_low), "You must input values for both 'between' entries."),
+        need(!is.na(input$between_high), "You must input values for both 'between' entries."),
+        need(!is.na(input$outside_low), "You must input values for both 'outside' entries."),
+        need(!is.na(input$outside_high), "You must input values for both 'outside' entries.")
+      )
+      if (input$areatype == "above")
+        auc <- pnorm(mean=input$mean, sd=input$sd, q=input$above, lower.tail=FALSE)
+      else if (input$areatype == "below")
+        auc <- pnorm(mean=input$mean, sd=input$sd, q=input$below)
+      else if (input$areatype == "between")
+        auc <- max(0, pnorm(mean=input$mean, sd=input$sd, q=input$between_high) - pnorm(mean=input$mean, sd=input$sd, q=input$between_low))
+      else if (input$areatype == "outside")
+        auc <- min(1, pnorm(mean=input$mean, sd=input$sd, q=input$outside_high, lower.tail=FALSE) + pnorm(mean=input$mean, sd=input$sd, q=input$outside_low))
+      
+      round(auc)
+    }
+  })
+  
+  output$areaout <- renderText(area())
+  
+  output$quantile <- renderText({
     if (input$areatype == "above")
-      auc <- pnorm(mean=input$mean, sd=input$sd, q=input$above, lower.tail=FALSE)
+      above()
     else if (input$areatype == "below")
-      auc <- pnorm(mean=input$mean, sd=input$sd, q=input$below)
+      below()
     else if (input$areatype == "between")
-      auc <- max(0, pnorm(mean=input$mean, sd=input$sd, q=input$between_high) - pnorm(mean=input$mean, sd=input$sd, q=input$between_low))
+      paste(round(between_low()), "and", round(between_high()))
     else if (input$areatype == "outside")
-      auc <- min(1, pnorm(mean=input$mean, sd=input$sd, q=input$outside_high, lower.tail=FALSE) + pnorm(mean=input$mean, sd=input$sd, q=input$outside_low))
-    
-    round(auc, digits=5)
+      paste(round(outside_low()), "and", round(outside_high()))
   })
   
   output$plot <- renderPlot({
@@ -54,7 +140,12 @@ shinyServer(function(input, output){
     if (area() > 0)
     {
       if (input$areatype == "outside")
-        g + geom_ribbon(data=region(), aes(ymax=y, group=group), ymin=0, fill="#409FED", alpha=0.4)
+      {
+#        labeldf <- data.frame(x=round(c(outside_low(), outside_high())), y=c(0, 0))
+        g + 
+          geom_ribbon(data=region(), aes(ymax=y, group=group), ymin=0, fill="#409FED", alpha=0.4)
+#          geom_text(data=labeldf, aes(x=x, y=y, label=x))
+      }
       else
         g + geom_ribbon(data=region(), aes(ymax=y), ymin=0, fill="#409FED", alpha=0.4)
     }
--- ui.r
+++ ui.r
@@ -11,45 +11,86 @@ shinyUI(
       "))
     ),
     
+    
     titlePanel("Normal Distribution"),
     plotOutput("plot"),
     hr(),
     
+    
+    fluidRow(
+      column(3,
+        radioButtons("inputtype", label=h3("Input Type"),
+            choices=list("Area from a value"="value", "Value from an area"="area"), 
+            selected="value")
+      ),
+      column(2,
+        br(),br(),
+        conditionalPanel(condition="input.inputtype == 'value'",
+          h5(strong("Area")),
+          uiOutput("areaout")
+        ),
+        conditionalPanel(condition="input.inputtype == 'area'",
+          numericInput(inputId="areain", label="Area", min=0, max=1, step=0.1, value=0.025)
+        )
+      )
+    ),
+    
+    
     fluidRow(
       column(2,
         numericInput(inputId="mean", label="Mean", value=0),
-        numericInput(inputId="sd", label="SD", value=1)
+        numericInput(inputId="sd", label="SD", min=0, step=1, value=1)
       ),
+      
       column(2,
         radioButtons("areatype", label=h3("Area Type"),
           choices=list("Above"="above", "Below"="below", "Between"="between", "Outside"="outside"), 
-          selected = "above")
+          selected="above")
       ),
+      
       column(5,
-        conditionalPanel(condition="input.areatype == 'above'",
-          numericInput(inputId="above", label="Above", value=1.96)
-        ),
-        
-        conditionalPanel(condition="input.areatype == 'below'",
-          numericInput(inputId="below", label="Below", value=-1.96)
-        ),
-        
-        conditionalPanel(condition="input.areatype == 'between'",
-          column(4,
-            numericInput(inputId="between_low", label="Lower", value=-1.96)
+        conditionalPanel(condition="input.inputtype == 'value'",
+          conditionalPanel(condition="input.areatype == 'above'",
+            numericInput(inputId="above", label="Above", value=1.96)
           ),
-          column(4,
-            numericInput(inputId="between_high", label="Upper", value=1.96)
+          
+          conditionalPanel(condition="input.areatype == 'below'",
+            numericInput(inputId="below", label="Below", value=-1.96)
+          ),
+          
+          conditionalPanel(condition="input.areatype == 'between'",
+            column(4,
+              numericInput(inputId="between_low", label="Lower", value=-1.96)
+            ),
+            column(4,
+              numericInput(inputId="between_high", label="Upper", value=1.96)
+            )
+          ),
+          
+          conditionalPanel(condition="input.areatype == 'outside'",
+            column(4,
+              numericInput(inputId="outside_low", label="Lower", value=-1.96)
+            ),
+            column(4,
+              numericInput(inputId="outside_high", label="Upper", value=1.96)
+            )
           )
         ),
         
-        conditionalPanel(condition="input.areatype == 'outside'",
-          column(4,
-            numericInput(inputId="outside_low", label="Lower", value=-1.96)
+        conditionalPanel(condition="input.inputtype == 'area'",
+          conditionalPanel(condition="input.areatype == 'above'",
+            h5(strong("Above"))
           ),
-          column(4,
-            numericInput(inputId="outside_high", label="Upper", value=1.96)
-          )
+          conditionalPanel(condition="input.areatype == 'below'",
+            h5(strong("Below"))
+          ),
+          conditionalPanel(condition="input.areatype == 'between'",
+            h5(strong("Between"))
+          ),
+          conditionalPanel(condition="input.areatype == 'outside'",
+            h5(strong("Outside"))
+          ),
+          uiOutput("quantile")
         )
       )
     )
